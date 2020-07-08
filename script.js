b7d5dac7a90624188e8f6daf584615eb4ab5f051
Irwin Litvak <irwin.litvak@gmail.com>
2020-07-08 15:44:15 -0400

    created backToTop button

--- script.js
+++ script.js
@@ -2,14 +2,52 @@
 /** Scroll To Section Button **/
 /******************************************/
 
-if($(window).width() > 1){
-    $(".scroller").click(function(e){
+$(document).ready(function(){
+
+    if($(window).width() > 1){
+        $(".scroller").click(function(e){
+            e.preventDefault();
+            console.log("works");
+            var aSelector = $(this).attr("href");
+            $("html, body").animate({
+                scrollTop: $(aSelector).offset().top
+            }, 1200);
+        })
+    } 
+
+    var buttonToTop = $('.back-to-top-container');
+
+    $(window).scroll(function() {    
+        var scrolled = $(window).scrollTop(),
+        documentHeight = $(document).height(),
+        windowHeight = $(window).height(),
+        halfWayPoint = (documentHeight - windowHeight) / 2
+    
+    
+        if (scrolled >= halfWayPoint - 100) {
+            $(buttonToTop).fadeIn(300);
+        } else {
+            $(buttonToTop).fadeOut(300);
+        }
+    });
+
+
+/******************************************/
+/** Back To Top Button **/
+/******************************************/
+
+
+    $('.back-to-top-container').click(function(e){
         e.preventDefault();
-        console.log("works");
-        var aSelector = $(this).attr("href");
+        var aSelector = $(this);
         $("html, body").animate({
-            scrollTop: $(aSelector).offset().top
+            scrollTop: 0
         }, 1200);
     })
-} 
+    
+})
+
+
+
+
 
